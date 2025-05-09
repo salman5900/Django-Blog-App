6573d40adfbe07f7ff93fbca7851ef8e2fa446b0
Salman Ahamed <salmanahamed5900@gmail.com>
2025-05-09 20:52:24 +0530

    changes made debug true for know for on progress

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*y^sm)^z#wd-iw93@vg&l@)d07sm-_9uk!$*4*+7ri+_e8xej0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
